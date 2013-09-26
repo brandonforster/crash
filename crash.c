136e463ed24a13e58fdb6adce7765af43ea83206
Brandon <brandonforster@gmail.com>
2013-09-26 17:07:14 -0400

    added .c skeleton

---
+++ crash.c
@@ -0,0 +1,15 @@
+// Brandon Forster
+// COP 4600- Operating Systems
+// Homework 1- Unix Terminal
+// 1 October 2013
+// crash.c
+// A crappy bash shell.
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+  // print the prompt
+  printf("# ");
+}
