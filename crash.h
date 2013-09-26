9c55e7b094b38d39d1c5c7bf20e4abfc6e37b673
brandonforster <brandonforster@gmail.com>
2013-09-26 17:26:12 -0400

    added make and empty header

---
+++ crash.h
@@ -0,0 +1,7 @@
+// Brandon Forster
+// COP 4600- Operating Systems
+// Homework 1- Unix Terminal
+// 1 October 2013
+// crash.h
+// A crappy bash shell.
+
---
+++ makefile
@@ -0,0 +1,2 @@
+crashmake: crash.c crash.h
+	gcc -o crash crash.c -I.
